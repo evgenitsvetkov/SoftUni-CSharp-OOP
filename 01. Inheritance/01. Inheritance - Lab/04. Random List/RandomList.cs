296f674d16d8003a0ca507c6916e8e2cf442abbf
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2023-12-24 16:37:56 +0200

    Add files via upload

---
+++ 01. Inheritance/01. Inheritance - Lab/04. Random List/RandomList.cs
@@ -0,0 +1,30 @@
+﻿namespace _04._Random_List
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            random = new Random();
+        }
+
+        public Random Random { get => random; set => random = value; }
+
+        public string RandomString()
+        {
+            if (Count == 0)
+            {
+                throw new ArgumentException("The list is empty");
+            }
+
+            int index = Random.Next(0, Count);
+            string stringIndex = this[index];
+
+            RemoveAt(index);
+
+            return stringIndex;
+
+        }
+    }
+}
---
+++ 01. Inheritance/01. Inheritance - Lab/04. Random List/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using _04._Random_List;
+
+namespace CustomRandomList
+{
+    public class StartUp 
+    {
+       static void Main()
+        {
+            RandomList randomList = new RandomList();
+            randomList.Add("Apple");
+            randomList.Add("Banana");
+            randomList.Add("Cherry");
+
+            Console.WriteLine("Random element: " + randomList.RandomString());
+
+            Console.ReadLine();
+        }
+    }
+
+}
