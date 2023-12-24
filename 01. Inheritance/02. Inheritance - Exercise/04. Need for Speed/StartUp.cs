5ef24bf16a5ebb6d0731cf07986cf7e5304e75fa
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2023-12-24 22:34:50 +0200

    Add files via upload

---
+++ 01. Inheritance/02. Inheritance - Exercise/04. Need for Speed/StartUp.cs
@@ -0,0 +1,19 @@
+﻿
+using NeedForSpeed;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        Car car = new(100, 100);
+        car.Drive(10);
+        System.Console.WriteLine(car.Fuel);
+
+        SportCar Sportcar = new(100, 100);
+        Sportcar.Drive(6);
+        System.Console.WriteLine(Sportcar.Fuel);
+
+    }
+
+
+}
---
+++ 01. Inheritance/02. Inheritance - Exercise/04. Need for Speed/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Runtime.InteropServices;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+
+
+
+        private const double DefaultFuelConsumption = 1.25;
+
+        public virtual double FuelConsumption
+            => DefaultFuelConsumption;
+
+        public double Fuel { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double kilometers)
+            => Fuel -= kilometers * FuelConsumption;
+
+        
+    }
+}
