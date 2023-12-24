e8926559e312bfc91b718ea1ec0041d5b4343a75
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2023-12-24 16:59:13 +0200

    Add files via update

---
+++ 01. Inheritance/02. Inheritance - Exercise/01. Person/Person.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace _01._Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get => name; 
+            set
+            {
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get => age; 
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Invalid age");
+                }
+                age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}", this.Name, this.Age));
+            
+            return stringBuilder.ToString().TrimEnd();      
+        }
+    }
+}
---
+++ 01. Inheritance/02. Inheritance - Exercise/01. Person/StartUp.cs
@@ -0,0 +1,12 @@
+﻿
+using _01._Person;
+
+static void Main()
+{
+    string name = Console.ReadLine();
+    int age = int.Parse(Console.ReadLine());
+
+    Child child = new Child(name, age);
+    Console.WriteLine(child);
+
+}
