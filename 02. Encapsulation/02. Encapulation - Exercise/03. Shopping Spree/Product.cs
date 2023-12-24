95fb39c031e819fb78e138954486e6ad7a2b642c
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2023-12-24 23:24:30 +0200

    Add files via upload

---
+++ 02. Encapsulation/02. Encapulation - Exercise/03. Shopping Spree/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSpree
+{
+    public class Product
+    {
+        private decimal cost;
+        private string name;
+
+        public Product(string name, decimal cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+
+        public string Name 
+        {
+            get => name; 
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+        public decimal Cost 
+        { 
+            get => cost;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.cost = value;
+            }
+
+        }
+    }
+}
---
+++ 02. Encapsulation/02. Encapulation - Exercise/03. Shopping Spree/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using ShoppingSpree;
+
+List<Person> people = new List<Person>();
+List<Product> products = new List<Product>();
+
+try
+{
+    string[] nameMoneyPairs = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+    foreach (var pair in nameMoneyPairs)
+    {
+        string[] nameMoney = pair.Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+        Person person = new(nameMoney[0], decimal.Parse(nameMoney[1]));
+
+        people.Add(person);
+    }
+
+    string[] productCostPairs = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+    foreach (var pair in productCostPairs)
+    {
+        string[] productCost = pair.Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+        Product product = new(productCost[0], decimal.Parse(productCost[1]));
+
+        products.Add(product);
+    }
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+
+    return;
+}
+
+string input;
+
+while ((input = Console.ReadLine()) != "END")
+{
+    string[] personProduct = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string personName = personProduct[0];
+    string productName = personProduct[1];
+
+    Person person = people.FirstOrDefault(p => p.Name == personName);
+    Product product = products.FirstOrDefault(p => p.Name == productName);
+
+    if (person is not null && product is not null)
+    {
+        Console.WriteLine(person.AddProduct(product));
+    }
+}
+
+foreach (var person in people)
+{
+    Console.WriteLine(person);
+}
