7a9894e408749b26129ccd9dfe3f90ffee5dc8b1
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2023-12-24 23:20:49 +0200

    Add files via upload

---
+++ 02. Encapsulation/02. Encapulation - Exercise/01. Class Box Data/StartUp.cs
@@ -0,0 +1,19 @@
+﻿//INPUT
+using ClassBoxData;
+
+try
+{
+    double length = double.Parse(Console.ReadLine());
+    double width = double.Parse(Console.ReadLine());
+    double height = double.Parse(Console.ReadLine());
+
+    Box box = new Box(length, width, height);
+
+    Console.WriteLine($"Surface Area - {box.Surface():f2}");
+    Console.WriteLine($"Lateral Surface Area - {box.LateralSurface():f2}");
+    Console.WriteLine($"Volume - {box.Volume():f2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
