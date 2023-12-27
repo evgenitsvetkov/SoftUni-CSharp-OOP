878ceccf7cd50ab663b62bb9ce9e1588b790f4b6
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2023-12-27 12:54:23 +0200

    Add files via upload

---
+++ 03. Interfaces and Abstraction/02. Interfaces and Abstraction - Exercise/04. Border Control/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using BorderControl.Models;
+using BorderControl.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+
+
+List<IIds> society = new();
+
+while (true)
+{
+	string input = Console.ReadLine();
+
+	if (input == "End")
+	{
+		break;
+	}
+
+	string[] tokens = input
+		.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+	if (tokens.Length == 3)
+	{
+		IIds person = new Person(tokens[2], tokens[0], int.Parse(tokens[1]));
+		society.Add(person);
+	}
+	else
+	{
+		IIds robot = new Robot(tokens[1], tokens[0]);
+		society.Add(robot);
+    }
+}
+
+string invalidNumber = Console.ReadLine();
+
+foreach (var element in society)
+{
+	if (element.Id.EndsWith(invalidNumber))
+	{
+		Console.WriteLine(element.Id);
+	}
+}
