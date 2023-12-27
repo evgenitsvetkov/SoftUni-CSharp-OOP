53d70a7eb58bccd7f66d2e8bea6f2b0df750ccec
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2023-12-27 12:53:06 +0200

    Add files via upload

---
+++ 03. Interfaces and Abstraction/02. Interfaces and Abstraction - Exercise/03. Telephony/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Telephony.Models;
+using Telephony.Models.Interfaces;
+
+string[] phoneNumbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+string[] urls = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+ICallable phone;
+
+foreach (var phoneNumber in phoneNumbers)
+{
+    if (phoneNumber.Length == 10)
+    {
+        phone = new Smartphone();
+    }
+    else
+    {
+        phone = new StationaryPhone();
+    }
+
+    try
+    {
+        Console.WriteLine(phone.Call(phoneNumber));
+    }
+    catch (Exception ex)
+    {
+
+        Console.WriteLine(ex.Message);
+    }
+}
+
+IBrowsable browse = new Smartphone();
+
+foreach (var url in urls)
+{
+    try
+    {
+        Console.WriteLine(browse.Browse(url));
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    
+}
