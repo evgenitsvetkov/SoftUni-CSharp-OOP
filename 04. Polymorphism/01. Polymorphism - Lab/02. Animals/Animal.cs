26c6aa60c31e43bde0331a8226a6a82426727dae
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2023-12-27 12:59:44 +0200

    Add files via upload

---
+++ 04. Polymorphism/01. Polymorphism - Lab/02. Animals/Animal.cs
@@ -0,0 +1,32 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favoriteFood;
+
+        protected Animal(string name, string favoriteFood)
+        {
+            this.Name = name;
+            this.FavoriteFood = favoriteFood;
+        }
+
+        public string Name 
+        { 
+            get => name; 
+            set => name = value; 
+        }
+
+        public string FavoriteFood 
+        { 
+            get => favoriteFood; 
+            set => favoriteFood = value; 
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my favourite food is {this.FavoriteFood}";
+        }
+
+    }
+}
