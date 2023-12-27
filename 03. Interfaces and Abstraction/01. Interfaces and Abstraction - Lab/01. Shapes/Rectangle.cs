1d74f8938206f02355efd6d5021e8aa35dd50fdf
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2023-12-27 12:47:17 +0200

    Add files via upload

---
+++ 03. Interfaces and Abstraction/01. Interfaces and Abstraction - Lab/01. Shapes/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int heigh)
+        {
+            Width = width;
+            Heigh = heigh;
+        }
+
+        public int Width { get; set; }
+
+        public int Heigh { get; set; }
+
+        public void Draw()
+        {
+            DrawLine(this.width, '*', '*');
+
+            for (int i = 1; i < this.height - 1; ++i)
+            {
+                DrawLine(this.width, '*', ' ');
+            }
+            DrawLine(this.width, '*', '*');
+        }
+
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.WriteLine(end);
+            for (int i = 1; i < width - 1; ++i)
+            {
+                Console.WriteLine(mid);
+            }
+            Console.WriteLine(end);
+        }
+    }
+}
