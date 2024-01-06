4a016189708b758cda18e532995f914b10418797
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-01-06 23:50:54 +0200

    Add files via upload

---
+++ 07. Reflection and Attributes/02. Reflection and Attributes - Exercise/01. Command Pattern/Core/Commands/ExitCommand.cs
@@ -0,0 +1,19 @@
+﻿
+using CommandPattern.Core.Contracts;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace CommandPattern.Core.Commands
+{
+    public class ExitCommand : ICommand
+    {
+        private const int DefaultExitCode = 0;
+
+        public string Execute(string[] args)
+        {
+            Environment.Exit(DefaultExitCode);
+
+            return null;
+        }
+    }
+}
