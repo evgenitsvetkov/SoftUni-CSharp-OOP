333192d9d5370e3187fbc743e48166aa4ddbd079
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-01-06 23:25:08 +0200

    Add files via upload

---
+++ 07. Reflection and Attributes/01. Reflection and Attributes - Lab/02. High Quality Mistakes/StartUp.cs
@@ -0,0 +1,12 @@
+﻿
+namespace HighQualityMistakes;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Spy spy = new Spy();
+        string result = spy.AnalyzeAccessModifiers("Hacker");
+        Console.WriteLine(result);
+    }
+}
